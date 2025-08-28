950e07dfd45dba529f87eff4100cf3b7ceb383da
Camilo Medina <medina1camilo@gmail.com>
2025-08-28 08:23:58 +0200

    [master] baseline

---
+++ src/main/java/co/medina/study/javakafkademo/JavaKafkaDemoApplication.java
@@ -0,0 +1,13 @@
+package co.medina.study.javakafkademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaKafkaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaKafkaDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/medina/study/javakafkademo/JavaKafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.medina.study.javakafkademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaKafkaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
