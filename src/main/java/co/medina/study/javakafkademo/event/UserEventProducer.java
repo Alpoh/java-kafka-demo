90753aa5928d813d8a9958a86c7212f4c7918732
Camilo Medina <medina1camilo@gmail.com>
2025-08-29 12:48:27 +0200

    [feature/user-crud] improve

--- src/main/java/co/medina/study/javakafkademo/event/UserEventProducer.java
+++ src/main/java/co/medina/study/javakafkademo/event/UserEventProducer.java
@@ -2,11 +2,14 @@ package co.medina.study.javakafkademo.event;
 
 import co.medina.study.javakafkademo.model.User;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserEventProducer {
 
     private static final String TOPIC = "users-events";
@@ -15,6 +18,6 @@ public class UserEventProducer {
 
     public void sendUserCreatedEvent(User user) {
         kafkaTemplate.send(TOPIC, user.getId().toString(), user);
-        System.out.println("User created event sent to Kafka: " + user);
+        log.info("User created event sent to Kafka: {}", user);
     }
 }
